5bae48e2408116b985c466eea85147a3ec5ac8ce
monoranjan.das <monoranjan.das@covianalytics.com>
2022-10-15 02:36:25 +0600

    create and design location page

---
+++ src/Controller/AdminsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminsController extends AbstractController
+{
+    #[Route('/admins', name: 'app_admins_location')]
+    public function index(): Response
+    {
+        return $this->render('admins/locations.html.twig', [
+            'controller_name' => 'AdminsController',
+        ]);
+    }
+
+    #[Route('/add', name: 'admins_add_location')]
+    public function addLocation(): Response
+    {
+        return $this->render('admins/add-locations.html.twig', [
+            'controller_name' => 'AdminsController',
+        ]);
+    }
+}
